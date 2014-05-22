c77ca65613135403c87ad953242f3c84df129c87
WangYiquan <cy_wyq@126.com>
2014-05-22 09:09:12 +0800

    将插件中的生成内部元素改为 data- 形式，避免id冲突问题

--- script/jquery.picMarque.js
+++ script/jquery.picMarque.js
@@ -12,35 +12,38 @@ $.fn.picMarque = function(t) {
   var n = {speed: 60,data:[], errorimg:'http://www.siaa.org.cn/style/common/nophoto.jpg'};
   var r = $.extend(n, t);
   var data = n.data;
-  var html = '<table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td id="marqueItem1"><div class="marque-main"><ul>'
+  var html = '<table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td data-marqueebox="1"><div class="marque-main"><ul>'
 
   for(var i=0;i<data.length;i++){
     if(i>=5) break;
     html  += '<li><a href="'+data[i].link+'"><img src="'+data[i].image+'" alt="'+data[i].title+'" onerror="this.src=\''+n.errorimg+'\'"></a><div class="splendid-links">'+data[i].title+'</div></li>';
   }
                  
-  html  +=  '</ul></div></td><td id="marqueItem2"></td></tr></tbody></table>';
+  html  +=  '</ul></div></td><td  data-marqueebox="2"></td></tr></tbody></table>';
 
   return $(this).each(function() {
-    $(this).append(html).addClass("marque-box");
-      var FGDemo = $(this).get(0);
-      var FGDemo1 = document.getElementById("marqueItem1");
-      var FGDemo2 = document.getElementById("marqueItem2");
-      FGDemo2.innerHTML = FGDemo1.innerHTML;
-      function Marquee1() {
-        if (FGDemo.scrollLeft >= FGDemo1.scrollWidth) {
-          FGDemo.scrollLeft = 0;
-        } else {
-          FGDemo.scrollLeft++;
-        }
+    if(data.length>0){
+      $(this).empty();
+      $(this).append(html).addClass("marque-box");
+    }
+    var FGDemo = $(this).get(0);
+    var FGDemo1 = $(this).find("[data-marqueebox='1']").get(0);
+    var FGDemo2 = $(this).find("[data-marqueebox='2']").get(0);
+    FGDemo2.innerHTML = FGDemo1.innerHTML;
+    function Marquee1() {
+      if (FGDemo.scrollLeft >= FGDemo1.scrollWidth) {
+        FGDemo.scrollLeft = 0;
+      } else {
+        FGDemo.scrollLeft++;
       }
+    }
 
-      var MyMar1 = setInterval(Marquee1, n.speed);
-      FGDemo.onmouseover = function () {
-        clearInterval(MyMar1);
-      }
-      FGDemo.onmouseout = function () {
-        MyMar1 = setInterval(Marquee1, n.speed);
-      } 
+    var MyMar1 = setInterval(Marquee1, n.speed);
+    FGDemo.onmouseover = function () {
+      clearInterval(MyMar1);
+    }
+    FGDemo.onmouseout = function () {
+      MyMar1 = setInterval(Marquee1, n.speed);
+    } 
   })
 }
